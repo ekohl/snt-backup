81b9c503bc87caa3f49993f0f32b1050f2ce7524
bas <bas@53864893-6df6-0310-a35a-92b7ff90ca16>
2005-06-15 10:53:03 +0000

    svn backup support
    git-svn-id: https://dev.snt.utwente.nl/svn/snt-syscom/snt-backup/trunk@17 53864893-6df6-0310-a35a-92b7ff90ca16

---
+++ root/usr/lib/snt-backup/bin/svn-incdump.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+sub assert(&) { unless (&$_[0]) { die "assertion failed!\n" }
+
+die "Usage: $0 <svnrepospath> <svnincfile>\n"
+	unless @ARGV == 2;
+
+my $repos = shift;
+my $f_rev = shift;
+
+my $head_rev = `svnlook youngest $repos`;
+chomp $head_rev;
+assert{ defined $head_rev && $head_rev =~ /^(0|[1-9]\d*)$/ };
+
+my $last_rev = 0;
+if (open REV, "< $f_rev") {
+	$last_rev = <REV>;
+	chomp $last_rev;
+	close REV;
+}
+
+assert{ defined $last_rev && $last_rev =~ /^(0|[1-9]\d*)$/ };
+assert{ $last_rev <= $head_rev };
+
+my $next_rev = $last_rev + 1;
+if ($next_rev <= $head_rev) {
+	system("svnadmin", "dump", "--incremental", "--quiet",
+			"--revision", "$next_rev:$head_rev", $repos)
+				or die "svnadmin dump failed: $?\n";
+
+	if (open REV, "> $f_rev") {
+		print REV, "$next_rev\n";
+		close REV;
+	}
+}
