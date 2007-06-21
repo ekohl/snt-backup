d9cf569a675eabe1bde843eeceb575ad7ad34795
rien <rien@53864893-6df6-0310-a35a-92b7ff90ca16>
2007-06-21 11:58:02 +0000

    config opties voor de --report optie van cleanup.pl
    git-svn-id: https://dev.snt.utwente.nl/svn/snt-syscom/snt-backup/trunk@60 53864893-6df6-0310-a35a-92b7ff90ca16

--- root/usr/lib/snt-backup/sbin/cleanup.sh
+++ root/usr/lib/snt-backup/sbin/cleanup.sh
@@ -6,6 +6,9 @@ export CONFDIR='/etc/snt-backup'
 . ${CONFDIR}/server/server.conf
 
 if [ "${BACKUP_LOCATION}x" = "x" ]; then BACKUP_LOCATION='/backups'; fi
+if [ "${NO_ACT}x" = "yesx" ]; then FLAGS="${FLAGS} --no-act"; fi
+if [ "${VERBOSE}x" = "yesx" ]; then FLAGS="${FLAGS} --verbose"; fi
+if [ "${REPORT}x" = "yesx" ]; then FLAGS="${FLAGS} --report"; fi
 
 cd ${BACKUP_LOCATION} || exit 1
 
@@ -14,7 +17,7 @@ do
 	(
 	        if [ ! -e "${i}/CLEANUP_DISABLE" ]; then 
   		        #echo "Cleaning $i..."
-		        cd "$i" && ${BASEDIR}/sbin/cleanup.pl
+		        cd "$i" && ${BASEDIR}/sbin/cleanup.pl ${FLAGS}
 		fi
 	)
 done
