2aac06382906492b7e1980cf876ec314e624eafa
bas <bas@53864893-6df6-0310-a35a-92b7ff90ca16>
2008-06-09 08:52:33 +0000

    - add day_weekly_cleanups option - fix mysql binary-log backupping
    git-svn-id: https://dev.snt.utwente.nl/svn/snt-syscom/snt-backup/trunk@144 53864893-6df6-0310-a35a-92b7ff90ca16

--- root/usr/lib/snt-backup/bin/backup.sh
+++ root/usr/lib/snt-backup/bin/backup.sh
@@ -49,31 +49,60 @@ export E_TAG=''
 # cd to snapshotdir
 cd "${SNAPSHOTDIR}" || exit 1
 
+if [ "$FUNC" = "weekly" ]; then
+	if [ -z "$DAY_WEEKLY_CLEANUP" ]; then
+		DOW=1
+	elif [ "$DAY_WEEKLY_CLEANUP" = "monday" ]; then
+		DOW=1
+	elif [ "$DAY_WEEKLY_CLEANUP" = "tuesday" ]; then
+		DOW=2
+	elif [ "$DAY_WEEKLY_CLEANUP" = "wednesday" ]; then
+		DOW=3
+	elif [ "$DAY_WEEKLY_CLEANUP" = "thursday" ]; then
+		DOW=4
+	elif [ "$DAY_WEEKLY_CLEANUP" = "friday" ]; then
+		DOW=5
+	elif [ "$DAY_WEEKLY_CLEANUP" = "saturday" ]; then
+		DOW=6
+	elif [ "$DAY_WEEKLY_CLEANUP" = "sunday" ]; then
+		DOW=7
+	else
+		echo "Invalid 'DAY_WEEKLY_CLEANUP'." >&2
+		exit 1
+	fi
+
+	# check day of week
+	if [ `date +%u` -ne $DOW ]; then
+		exit 0
+	fi
+fi
+
 # reset de error flag..
 rm -f "${SNAPSHOTDIR}/error"
 
 (
-        if [ "${LOAD_PLUGINS}x" = "x" ]; then
-                # Als niet gedefinieerd is welke plugins we willen laden, dan laden we ze allemaal automagisch
-    	        # som alle uitvoerbare plugins in de plugins dir op.. (met +x)
-    	        PLUGINS="`cd "${BASEDIR}/plugins" && find . -type f -perm +100 -a ! -name '*.dpkg-*' | sed 's|^\./||'`"
-        else
-                # Anders halen we het uit de config
-	        PLUGINS=""
-	        for plugin in ${LOAD_PLUGINS}; do
-	                if [ ! -x ${BASEDIR}/plugins/${plugin} ]; then
-	    	                echo "Error: Plugin ${plugin} not found" 1>&2
+		if [ "${LOAD_PLUGINS}x" = "x" ]; then
+				# Als niet gedefinieerd is welke plugins we willen laden,
+				# dan laden we ze allemaal automagisch
+				# som alle uitvoerbare plugins in de plugins dir op.. (met +x)
+				PLUGINS="`cd "${BASEDIR}/plugins" && find . -type f -perm +100 -a ! -name '*.dpkg-*' | sed 's|^\./||'`"
+		else
+			# Anders halen we het uit de config
+			PLUGINS=""
+			for plugin in ${LOAD_PLUGINS}; do
+					if [ ! -x ${BASEDIR}/plugins/${plugin} ]; then
+							echo "Error: Plugin ${plugin} not found" 1>&2
 				touch "${SNAPSHOTDIR}/error"
-	                else
-		                PLUGINS="${PLUGINS} ${plugin}"
-	                fi
-	        done
-        fi
+					else
+						PLUGINS="${PLUGINS} ${plugin}"
+					fi
+			done
+		fi
 
-        # laad alle scripts in
-        for plugin in ${PLUGINS}; do
-	    echo "Loading plugin '${plugin}'."
-	    . "${BASEDIR}/plugins/${plugin}"
+		# laad alle scripts in
+		for plugin in ${PLUGINS}; do
+		echo "Loading plugin '${plugin}'."
+		. "${BASEDIR}/plugins/${plugin}"
 	done
 		
 	# en voer de juiste functie uit
@@ -97,9 +126,9 @@ rm -f "${SNAPSHOTDIR}/error"
 
 # is de error-flag geset?
 if [ -e "${SNAPSHOTDIR}/error" -o -s "${SNAPSHOTDIR}/${DATEFIX}-stderr" ]; then
-    ERROR=1
+	ERROR=1
 else
-    ERROR=0
+	ERROR=0
 fi
 
 if [ $ERROR = 1 -o "`echo ${REPORT_ALWAYS} | tr A-Z a-z`x" = "yesx" ]; then
