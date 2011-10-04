3495ff32aa4129732cd4d035742eedd6912d5242
bas <bas@53864893-6df6-0310-a35a-92b7ff90ca16>
2011-10-04 08:09:21 +0000

    - assume Digest::MD5 is always available (since debian lenny no longer a separate package) - add more robustness: improve handling of race conditions - cleanup locks when not all locks could be acquired - improve error handling
    git-svn-id: https://dev.snt.utwente.nl/svn/snt-syscom/snt-backup/trunk@360 53864893-6df6-0310-a35a-92b7ff90ca16

--- root/usr/bin/lock.pl
+++ root/usr/bin/lock.pl
@@ -1,11 +1,11 @@
-#! /usr/bin/perl -w
+#!/usr/bin/perl -w
 #
 # Author: Bas van Sisseren <bas@snt.utwente.nl>
 
 use strict;
-use Fcntl;
-
-my $use_md5 = eval { require Digest::MD5 };
+use Digest::MD5 qw/ md5_hex /;
+use Fcntl qw/ O_RDONLY O_WRONLY O_CREAT O_EXCL /;
+use Errno qw/ EEXIST /;
 
 my @lockfiles = ();
 my $acquire_time = 60;
@@ -13,120 +13,106 @@ my $lock_all = 1;
 
 while ((@ARGV) && ($ARGV[0] =~ /^-/)) {
 	my $param = shift @ARGV;
-	if (($param =~ /^-l$/) || ($param =~ /^--lockfile$/)) {
-		unless (@ARGV) {
-			print STDERR "Invalid parameter.\n";
-			exit 1;
-		}
+	if (($param =~ /^-l\z/) || ($param =~ /^--lockfile\z/)) {
+		die "Invalid parameter.\n" unless @ARGV;
 		push @lockfiles, shift @ARGV;
 
-	} elsif (($param =~ /^-l=(.*)$/) || ($param =~ /^--lockfile=(.*)$/)) {
+	} elsif (($param =~ /^-l=(.*)\z/) || ($param =~ /^--lockfile=(.*)\z/)) {
 		push @lockfiles, $1;
 
-	} elsif (($param =~ /^-a$/) || ($param =~ /^--acquire-time$/)) {
-		unless (@ARGV) {
-			print STDERR "Invalid parameter.\n";
-			exit 1;
-		}
+	} elsif (($param =~ /^-a\z/) || ($param =~ /^--acquire-time\z/)) {
+		die "Invalid parameter.\n" unless @ARGV;
 		$acquire_time = shift @ARGV;
 
-	} elsif (($param =~ /^-a=(.*)$/) || ($param =~ /^--acquire-time=(.*)$/)) {
+	} elsif (($param =~ /^-a=(.*)\z/) || ($param =~ /^--acquire-time=(.*)\z/)) {
 		$acquire_time = $1;
 
 	} elsif ($param eq '--one-lock') {
 		$lock_all = 0;
 
 	} else {
-		print STDERR "Unknown parameter: '$param'.\n";
+		die "Unknown parameter: '$param'.\n";
 	}
 }
 
 die "Invalid lock-acquire time: '$acquire_time'.\n"
 	unless $acquire_time =~ /^[1-9]\d*$/;
 
-unless (@ARGV) {
-	print STDERR "Usage: $0 [--lockfile <lockfile>] [--acquire-time <seconds>] <command> <args..>\n";
-	exit 0;
-}
+die "Usage: $0 [--lockfile <lockfile>] [--acquire-time <seconds>] <command> <args..>\n"
+	unless @ARGV;
 
-unless (@lockfiles) {
-	my $hash = join "\0", @ARGV;
-	if ($use_md5) {
-		$hash = Digest::MD5::md5_hex($hash);
-	} else {
-		my $hash2 = "\0" x 16;
-		while ($hash =~ s/^(.{1,16})//s) { $hash2 ^= $1 }
-		$hash = unpack 'H*', $hash2;
-	}
-	push @lockfiles, '/var/lock/.lockfile-'.$hash;
-}
+push @lockfiles, '/var/lock/.lockfile-'.md5_hex(join "\0", @ARGV)
+	unless @lockfiles;
 
 foreach my $lockfile (@lockfiles) {
 	if (sysopen LOCK, $lockfile, O_RDONLY) {
-		my $pid = <LOCK>;
-		$pid = '' unless defined $pid;
+		my $pid = <LOCK> // '';
 		my $read_retries = 10;
 		while ($read_retries-- && $pid !~ /\n/) {
 			# we're too fast..
 			select(undef,undef,undef,0.01);
-			my $pid2 = <LOCK>;
-			$pid2 = '' unless defined $pid2;
-			$pid .= $pid2;
+			$pid .= <LOCK> // '';
 		}
 		close LOCK;
 
 		chomp $pid;
-		if ($pid =~ /^[1-9]\d*$/) {
+		if ($pid =~ /\A[1-9]\d*?\z/) {
 			unless ( -d "/proc/$pid" ) {
 				# reopen lockfile; same pid?
 				if (sysopen LOCK, $lockfile, O_RDONLY) {
-					my $pid2 = <LOCK>;
-					$pid2 = '' unless defined $pid2;
+					my $pid2 = <LOCK> // '';
 					close LOCK;
 
 					chomp $pid2;
 					if ($pid eq $pid2) {
-						print STDERR "Process locked: PID $pid not found (lockfile '$lockfile').\n";
+						warn "Process locked: PID $pid not found (lockfile '$lockfile').\n";
 					} # else 'old lock finished; lockfile claimed by next process'
 				} # else 'old lock finished; lockfile removed'
 			}
 		} else {
-			print STDERR "Process locked: No PID in lockfile found (lockfile '$lockfile').\n";
+			warn "Process locked: No PID in lockfile found (lockfile '$lockfile').\n";
 		}
 	}
 }
 
 # lock
 my $now = time;
-my @lock_queue = @lockfiles;
+my @lock_queue = ( @lockfiles, undef );
 my @lock_done = ();
 while (@lock_queue) {
 	my $lockfile = shift @lock_queue;
 
-	if (sysopen LOCK, $lockfile, O_CREAT | O_EXCL | O_WRONLY, 0600) {
+	if (!defined $lockfile) {
+		if (@lock_queue) {
+			push @lock_queue, undef;
+			select(undef, undef, undef, 0.1);
+		}
+
+	} elsif (sysopen LOCK, $lockfile, O_CREAT | O_EXCL | O_WRONLY, 0600) {
 		print LOCK "$$\n";
 		close LOCK;
 		push @lock_done, $lockfile;
 		last unless $lock_all;
 
-	} elsif ($!{EEXIST}) {
+	} elsif ($! == EEXIST) {
 		push @lock_queue, $lockfile;
 		if ($now + $acquire_time <= time) {
 			# lock failed..
-			print STDERR "Process locked: Lock not released after $acquire_time secs (lockfile '$lockfile').\n";
+			warn "Process locked: Lock not released after $acquire_time secs (lockfile '$lockfile').\n";
 			foreach my $lockfile (@lock_done) {
 				unlink $lockfile;
 			}
 			exit 1;
 		}
 
-		select(undef,undef,undef,0.01);
-
 	} else {
-		die "sysopen('$lockfile'): $!\n";
+		warn "[lock] sysopen('$lockfile'): $!\n";
+		foreach my $lockfile (@lock_done) {
+			unlink $lockfile;
+		}
+		exit 1;
 	}
 }
-
 
 # call command..
 system(@ARGV);
@@ -134,7 +120,8 @@ my $exitlvl = $?;
 
 # unlock
 foreach my $lockfile (@lock_done) {
-	unlink $lockfile;
+	unlink $lockfile
+		or warn "[lock] unlink('$lockfile'): $!\n";
 }
 
 if ($exitlvl == 0) {
