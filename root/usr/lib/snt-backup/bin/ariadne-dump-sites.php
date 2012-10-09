4a236da545a86b4ebf107c6e250a891b76c67247
muller <muller@53864893-6df6-0310-a35a-92b7ff90ca16>
2012-10-09 13:20:26 +0000

    Adding support for adding a filter argument in the scan folder entry for customized query's in ariadne
    git-svn-id: https://dev.snt.utwente.nl/svn/snt-syscom/snt-backup/trunk@438 53864893-6df6-0310-a35a-92b7ff90ca16

--- root/usr/lib/snt-backup/bin/ariadne-dump-sites.php
+++ root/usr/lib/snt-backup/bin/ariadne-dump-sites.php
@@ -2,6 +2,14 @@
 <?php
 	$ariadne = $argv[1].'/lib/';
 	$dumpdir = $argv[2];
+	$sitequery   = $argv[3];
+
+	if(!isset($argv[3]) || $argv[3] == "") {
+		$sitequery = "object.type = 'psite' ";
+	} else {
+		$sitequery   = $argv[3];
+	}
+
 	if(!is_dir($ariadne)){
 		echo "geef een ariadne dir mee als argument\n";
 		exit(0);
@@ -17,10 +25,11 @@
 			exit(1);
 		}
 	}
-	require($ariadne."/configs/store.phtml");
-	require($ariadne."/includes/loader.cmd.php");
-	require($ariadne."/stores/".$store_config["dbms"]."store.phtml");
+	require_once($ariadne."/configs/store.phtml");
+	require_once($ariadne."/includes/loader.cmd.php");
+	require_once($ariadne."/stores/".$store_config["dbms"]."store.phtml");
 	include($ariadne."/nls/".$AR->nls->default);
+	require_once($ariadne."/ar.php");
 
 	/* instantiate the store */
 	$inst_store = $store_config["dbms"]."store";
@@ -33,7 +42,7 @@
 	$AR->user = current($store->call('system.get.phtml', '', $store->find('/system/users/', $query)));
 
 	/* do your stuff here */
-	$paths = $store->call('system.get.path.phtml', '', $store->find($dumpdir,"object.type = 'psite' ",0,0));
+	$paths = $store->call('system.get.path.phtml', '', $store->find($dumpdir,$sitequery ,0,0));
 	echo implode("\n",$paths)."\n";
 
 	$store->close();
