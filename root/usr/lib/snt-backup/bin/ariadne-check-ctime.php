dd2c21ca485323fc9bda6a8d49718ef254d5a19f
muller <muller@53864893-6df6-0310-a35a-92b7ff90ca16>
2015-03-01 13:50:32 +0000

    ariadne 9 compatibility
    git-svn-id: https://dev.snt.utwente.nl/svn/snt-syscom/snt-backup/trunk@526 53864893-6df6-0310-a35a-92b7ff90ca16

--- root/usr/lib/snt-backup/bin/ariadne-check-ctime.php
+++ root/usr/lib/snt-backup/bin/ariadne-check-ctime.php
@@ -1,5 +1,6 @@
-#!/usr/bin/php -q
+#!/usr/bin/env php
 <?php
+
 	$ariadne = $argv[1].'/lib/';
 	$site = $argv[2];
 	$mtime = $argv[3];
@@ -11,22 +12,31 @@
 		echo "site required\n";
 		exit(0);
 	}
-	if (!@include_once($ariadne."/configs/ariadne.phtml")) {
-		chdir(substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')));
-		if(!include_once($ariadne."/configs/ariadne.phtml")){
-			echo "could not open ariadne.phtml";
-			exit(1);
+
+	$ARLoader = 'cmd';
+	$currentDir = getcwd();
+
+	if (file_exists($ariadne."/bootstrap.php")) {
+		// ariadne 9.0
+		require_once($ariadne."/bootstrap.php");
+	} else {
+		// ariadne pre 9.0
+		if (!@include_once($ariadne."/configs/ariadne.phtml")) {
+			chdir(substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')));
+			if(!include_once($ariadne."/configs/ariadne.phtml")){
+				echo "could not open ariadne.phtml";
+				exit(1);
+			}
 		}
+		require($ariadne."/configs/store.phtml");
+		require($ariadne."/includes/loader.cmd.php");
+		require($ariadne."/stores/".$store_config["dbms"]."store.phtml");
+		include($ariadne."/nls/".$AR->nls->default);
+
 	}
-	require($ariadne."/configs/store.phtml");
-	require($ariadne."/includes/loader.cmd.php");
-	require($ariadne."/stores/".$store_config["dbms"]."store.phtml");
-	include($ariadne."/nls/".$AR->nls->default);
 
-	/* instantiate the store */
 	$inst_store = $store_config["dbms"]."store";
-	$store = new $inst_store($root,$store_config);
-
+	$store=new $inst_store($root,$store_config);
 
 	/* now load a user (admin in this case)*/
 	$login = "admin";
@@ -34,7 +44,7 @@
 	$AR->user = current($store->call('system.get.phtml', '', $store->find('/system/users/', $query)));
 
 	/* do your stuff here */
-	$count = count($store->call('system.get.path.phtml', '', $store->find($site," time.mtime > $mtime order by time.mtime")));
+	$count = $store->count_find($site," time.mtime > $mtime ");
 
 	$store->close();
 	exit($count?1:0);
--- root/usr/lib/snt-backup/bin/ariadne-dump-sites.php
+++ root/usr/lib/snt-backup/bin/ariadne-dump-sites.php
@@ -1,4 +1,4 @@
-#!/usr/bin/php -q
+#!/usr/bin/env php
 <?php
 	$ariadne = $argv[1].'/lib/';
 	$dumpdir = $argv[2];
@@ -18,18 +18,30 @@
 		echo "dumpdir required\n";
 		exit(0);
 	}
-	if (!@include_once($ariadne."/configs/ariadne.phtml")) {
-		chdir(substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')));
-		if(!include_once($ariadne."/configs/ariadne.phtml")){
-			echo "could not open ariadne.phtml";
-			exit(1);
+
+
+	$ARLoader = 'cmd';
+	$currentDir = getcwd();
+
+	if (file_exists($ariadne."/bootstrap.php")) {
+		// ariadne 9.0
+		require_once($ariadne."/bootstrap.php");
+	} else {
+		// ariadne pre 9.0
+		if (!@include_once($ariadne."/configs/ariadne.phtml")) {
+			chdir(substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/')));
+			if(!include_once($ariadne."/configs/ariadne.phtml")){
+				echo "could not open ariadne.phtml";
+				exit(1);
+			}
 		}
+		require($ariadne."/configs/store.phtml");
+		require($ariadne."/includes/loader.cmd.php");
+		require($ariadne."/stores/".$store_config["dbms"]."store.phtml");
+		include($ariadne."/nls/".$AR->nls->default);
+
 	}
-	require_once($ariadne."/configs/store.phtml");
-	require_once($ariadne."/includes/loader.cmd.php");
-	require_once($ariadne."/stores/".$store_config["dbms"]."store.phtml");
-	include($ariadne."/nls/".$AR->nls->default);
-	require_once($ariadne."/ar.php");
+
 
 	/* instantiate the store */
 	$inst_store = $store_config["dbms"]."store";
