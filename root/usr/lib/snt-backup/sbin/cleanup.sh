3a5c827afd5556addf79580a196ea1280b6aefa6
muller <muller@53864893-6df6-0310-a35a-92b7ff90ca16>
2015-03-10 09:46:47 +0000

    add cleanup configuration per backup user
    git-svn-id: https://dev.snt.utwente.nl/svn/snt-syscom/snt-backup/trunk@531 53864893-6df6-0310-a35a-92b7ff90ca16

--- root/usr/lib/snt-backup/sbin/cleanup.sh
+++ root/usr/lib/snt-backup/sbin/cleanup.sh
@@ -6,21 +6,25 @@ export CONFDIR='/etc/snt-backup'
 . ${CONFDIR}/server/server.conf
 
 if [ "${BACKUP_LOCATION}x" = "x" ]; then BACKUP_LOCATION='/backups'; fi
-if [ "${NO_ACT}x" = "yesx" ]; then FLAGS="${FLAGS} --no-act"; fi
-if [ "${VERBOSE}x" = "yesx" ]; then FLAGS="${FLAGS} --verbose"; fi
-if [ "${REPORT}x" = "yesx" ]; then FLAGS="${FLAGS} --report"; fi
-if [ "${RETENTION_PERIOD_DAILY}x" != "x" ]; then FLAGS="${FLAGS} --retention_period_daily=${RETENTION_PERIOD_DAILY}"; fi
-if [ "${RETENTION_PERIOD_MONTHLY}x" != "x" ]; then FLAGS="${FLAGS} --retention_period_monthly=${RETENTION_PERIOD_MONTHLY}"; fi
-if [ "${RETENTION_PERIOD_YEARLY}x" != "x" ]; then FLAGS="${FLAGS} --retention_period_yearly=${RETENTION_PERIOD_YEARLY}"; fi
 
 cd ${BACKUP_LOCATION} || exit 1
 
 for i in */backups
 do
-	(
-	        if [ ! -e "${i}/CLEANUP_DISABLE" ]; then 
-  		        #echo "Cleaning $i..."
-		        cd "$i" && ${BASEDIR}/sbin/cleanup.pl ${FLAGS}
-		fi
-	)
+(
+	CONFIG="`dirname ${i}`/etc/cleanup.conf"
+	if [ -f "${CONFIG}" ] ; then
+		. "${CONFIG}"
+	fi
+	if [ "${NO_ACT}x" = "yesx" ]; then FLAGS="${FLAGS} --no-act"; fi
+	if [ "${VERBOSE}x" = "yesx" ]; then FLAGS="${FLAGS} --verbose"; fi
+	if [ "${REPORT}x" = "yesx" ]; then FLAGS="${FLAGS} --report"; fi
+	if [ "${RETENTION_PERIOD_DAILY}x" != "x" ]; then FLAGS="${FLAGS} --retention_period_daily=${RETENTION_PERIOD_DAILY}"; fi
+	if [ "${RETENTION_PERIOD_MONTHLY}x" != "x" ]; then FLAGS="${FLAGS} --retention_period_monthly=${RETENTION_PERIOD_MONTHLY}"; fi
+	if [ "${RETENTION_PERIOD_YEARLY}x" != "x" ]; then FLAGS="${FLAGS} --retention_period_yearly=${RETENTION_PERIOD_YEARLY}"; fi
+	if [ ! -e "${i}/CLEANUP_DISABLE" ]; then 
+		#echo "Cleaning $i..."
+		cd "$i" && ${BASEDIR}/sbin/cleanup.pl ${FLAGS}
+	fi
+)
 done
